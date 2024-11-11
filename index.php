c25df11319107d82024ae84d801e1b9e7e113938
phuhieu22 <hieudpph48709@fpt.edu.vn>
2024-11-11 21:29:21 +0700

    lần đầu em chơi với git

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // $name = "hiếu";
+    // //hiếu
+    // //tên của tôi là hiếu
+    // echo $name."<br>"; 
+    // echo "tên của tôi là $name"."<br>";
+
+    // $a=$b=$c=20;
+    // echo $a."<br>";
+    // echo $b."<br>";
+    // echo $c."<br>";
+
+    // $x=5;
+    // $y=10;
+    // $s = $x+$y;
+    // echo $x+$y. "<br>";
+    // echo "Diem cua tôi là".$x+$y ."<br>";
+    // echo "diem cua tôi là $s";
+
+    $name = "Hiếu";
+    $num =10;
+    $pi = 3.14;
+    $bool = true;
+    $arr = [10, 3.14 , false,"Hieu"];
+    //in ra màn hình check kiểu dữ liệu 
+    var_dump($name);
+    echo "<br>";
+    var_dump($num);
+    echo "<br>";
+    var_dump($pi);
+    echo "<br>";
+    var_dump($bool);
+    echo "<br>";
+    var_dump($arr);
+    echo "<br>";
+    
+    //bai1
+    $name = "hiếu";
+    $age = 30;
+    echo "tên của tôi là $name" ."<br>";
+    echo "Tuổi hiện tại của tôi là $age". "<br>";
+
+    $x = 10;;
+    $pi = 3.14;
+    $condeStudent = "Ph48709";
+    $arr = [1,2,3];
+    var_dump($x);
+    echo "<br>";
+    var_dump($pi);
+    echo "<br>";
+    var_dump($condeStudent);
+    echo "<br>";
+    var_dump($arr);
+    echo "<br>";
+    ?>
+</body>
+</html>
